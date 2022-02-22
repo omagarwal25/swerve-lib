48b52242faab9a78c7143425263c2b719612101a
omagarwal25 <lidagarwal@gmail.com>
2022-02-22 11:43:54 +0000

    fixed ratioed

--- src/main/java/com/omagarwal25/swervelib/ctre/CanCoderFactoryBuilder.java
+++ src/main/java/com/omagarwal25/swervelib/ctre/CanCoderFactoryBuilder.java
@@ -8,7 +8,7 @@ import com.omagarwal25.swervelib.AbsoluteEncoder;
 import com.omagarwal25.swervelib.AbsoluteEncoderFactory;
 
 public class CanCoderFactoryBuilder {
-    //    private Direction direction = Direction.COUNTER_CLOCKWISE;
+    // private Direction direction = Direction.COUNTER_CLOCKWISE;
     private int periodMilliseconds = 10;
 
     public CanCoderFactoryBuilder withReadingUpdatePeriod(int periodMilliseconds) {
@@ -17,7 +17,7 @@ public class CanCoderFactoryBuilder {
     }
 
     public CanCoderFactoryBuilder withDirection(Direction direction) {
-//        this.direction = direction;
+        // this.direction = direction;
         return this;
     }
 
@@ -25,13 +25,15 @@ public class CanCoderFactoryBuilder {
         return configuration -> {
             TalonFXConfiguration config = new TalonFXConfiguration();
             config.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
-//            config.magnetOffsetDegrees = Math.toDegrees(configuration.getOffset());
-//            config.sensorDirection = direction == Direction.CLOCKWISE;
+            // config.magnetOffsetDegrees = Math.toDegrees(configuration.getOffset());
+            // config.sensorDirection = direction == Direction.CLOCKWISE;
 
             TalonFX encoder = new TalonFX(configuration.getId());
             CtreUtils.checkCtreError(encoder.configAllSettings(config, 250), "Failed to configure CANCoder");
 
-            CtreUtils.checkCtreError(encoder.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, periodMilliseconds, 250), "Failed to configure Talon update rate");
+            CtreUtils.checkCtreError(
+                    encoder.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, periodMilliseconds, 250),
+                    "Failed to configure Talon update rate");
 
             return new EncoderImplementation(encoder);
         };
@@ -47,7 +49,7 @@ public class CanCoderFactoryBuilder {
         @Override
         public double getAbsoluteAngle() {
             // check this if this works
-            double angle = Math.toRadians(encoder.getSelectedSensorPosition());
+            double angle = Math.toRadians(encoder.getSelectedSensorPosition()) / 12.8;
             angle %= 2.0 * Math.PI;
             if (angle < 0.0) {
                 angle += 2.0 * Math.PI;
